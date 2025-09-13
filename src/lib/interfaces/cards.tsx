235995557520937851e63c1e37e0f0f6f0ab481a
Simón Gerardo Flores <simonflores_10@hotmail.com>
2025-09-13 13:30:55 +0200

    section 1 is working

---
+++ src/lib/interfaces/cards.tsx
@@ -0,0 +1,24 @@
+interface ImportantWord {
+  german: string;
+  spanish: string;
+}
+
+interface Question {
+  title: string;
+  text: string; 
+  difficulty: number; 
+  importantWords: ImportantWord[];
+}
+
+interface Answer {
+  title: string; 
+  text: string; 
+  importantWords: ImportantWord[];
+}
+
+export interface Card {
+  number: string; 
+  section: string; 
+  question: Question;
+  answer: Answer;
+}
